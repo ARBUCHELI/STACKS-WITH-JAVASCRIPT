10e0934030fa7071a5efccdc53660aa259d2d7cf
ARBUCHELI <radio_seattle@hotmail.com>
2023-02-26 01:43:44 -0500

    push and pop

--- Stack.js
+++ Stack.js
@@ -4,6 +4,16 @@ class Stack {
   constructor() {
     this.stack = new LinkedList();
   }
+
+  push(value) {
+    this.stack.addToHead(value);
+  }
+
+  pop() {
+    const value = this.stack.removeHead();
+    return value;
+  }
+
   peek() {
     return this.stack.head.data;
   }
