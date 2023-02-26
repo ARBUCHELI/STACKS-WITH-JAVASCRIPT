977095ddcdbf4bdea5aaea0fe14b2e75b6d5ceb7
ARBUCHELI <radio_seattle@hotmail.com>
2023-02-26 02:12:23 -0500

    size ii

--- Stack.js
+++ Stack.js
@@ -3,33 +3,46 @@ const LinkedList = require('./LinkedList');
 class Stack {
   constructor(maxSize = Infinity) {
     this.stack = new LinkedList();
-    this.size = 0;
     this.maxSize = maxSize;
+    this.size = 0;
+  }
+
+  hasRoom() {
+    return this.size < this.maxSize;
+  }
+
+  isEmpty() {
+    return this.size === 0;
   }
 
   push(value) {
-    this.stack.addToHead(value);
+    if (this.hasRoom()) {
+      this.stack.addToHead(value);
+      this.size++;
+    } else {
+        throw new Error('Stack is full')
+    }
   }
 
   pop() {
-    if (this.size > 0) {
-       const value = this.stack.removeHead();
-       this.size--;
-       return value;
+    if (!this.isEmpty()) {
+      const value = this.stack.removeHead();
+      this.size--;
+      return value;
     }
     else {
-      console.log('Stack is empty');
+      console.log('Stack is empty!');
     }
   }
 
   peek() {
-    if (this.size > 0) {
-       return this.stack.head.data;
-    }
-    else {
+    if (!this.isEmpty()) {
+      return this.stack.head.data;
+    } else {
       return null;
     }
   }
+
 }
 
 module.exports = Stack;
